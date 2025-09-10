51c05b0de4ccad187c40d3051f8dd93cf3004ec1
SifanGeremu <wins33975@gmail.com>
2025-09-09 21:43:30 -0500

    add .env.example and api setup

--- clientside/src/Services/api.js
+++ clientside/src/Services/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "b4ac36911a674f0d887a4266b2dbf081";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
